590c195037f7f214588a645ca096cdf33e028948
John Drake <je.drake@westernsydney.edu.au>
2017-03-17 14:25:08 +1100

    added fine root production module

--- run.R
+++ run.R
@@ -5,7 +5,6 @@ lai_variable <- make_lai_variable()
 
 sla_variable <- make_sla_variable()
 
-
 leaf_pool <- make_leaf_pool(lai_variable, sla_variable)
 
 soil_respiration_flux <- make_soil_respiration_flux()
@@ -13,3 +12,5 @@ soil_respiration_flux <- make_soil_respiration_flux()
 soil_carbon_pool <- make_soil_carbon_pool()
 
 fineroot_pool <- make_fineroot_pool(c_fraction)
+
+fineroot_production_flux <- make_fineroot_production_flux(c_fraction)
