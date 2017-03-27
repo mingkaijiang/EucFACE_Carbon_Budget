d79438f0d3176889ead74bc1d0304892091acaab
John Drake <je.drake@westernsydney.edu.au>
2017-03-28 01:43:14 +1100

    Return a number for "Ring" in LAI module

--- modules/lai_variable/make_lai_variable.R
+++ modules/lai_variable/make_lai_variable.R
@@ -5,6 +5,9 @@ make_lai_variable <- function(){
   res <- subset(res, select=c(Date, Ring, LAI))
   names(res)[3] <- "lai_variable"
   
+  #- return a number for ring
+  res$Ring <- as.numeric(res$Ring)
+  
   return(res)
 }
 
