856df89f4f197abdad796df954f40f588cdbbcd6
mingkaijiang <shanyujordan512@gmail.com>
2017-09-11 11:39:10 +1000

    DOC leaching start and end date added. Currently start date is the same as the end date. the representation of drainage is simplified.

--- run.R
+++ run.R
@@ -21,14 +21,13 @@ herbivory_leaf_consumption_flux <- make_herbivory_leaf_consumption_flux(sla_vari
 
 lerp_production_flux <- make_lerp_production_flux()
 
-# incomplete  - needs a water flux, possibly simulated
-# doc_leaching_flux <- make_doc_leaching_flux()
-
+doc_leaching_flux <- make_doc_leaching_flux()
 
 # Litter fluxes. This dataframe includes all of twig, bark, seed, leaf. 
 leaflitter_flux <- make_leaflitter_flux(c_fraction)
 
 wood_pool <- make_wood_pool(ring_area,c_fraction)
+
 wood_production_flux <- make_wood_production_flux(wood_pool)
 
 # Second method for the wood pool.
