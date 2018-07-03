6f97fe4b0b799057702fd8839c7caeb532f13ffd
mingkaijiang <shanyujordan512@gmail.com>
2018-07-03 14:44:24 +1000

    should be able to run the stats for absolute diffs and check the results.

--- R/generate_stats_abs.R
+++ R/generate_stats_abs.R
@@ -8,52 +8,65 @@ generate_stats_abs <- function() {
     #### Work on each variable per time
     ### LAI
     s.lai <- treatment_effect_abs_statistics(inDF=lai_variable, 
-                                         var.cond="flux", var.col=3)
+                                         var.cond="pool", var.col=3,
+                                         date.as.factor=T)
     
     ### SLA 
     s.sla <- treatment_effect_abs_statistics(inDF=sla_variable, 
-                                         var.cond="flux", var.col=3)
+                                         var.cond="pool", var.col=3,
+                                         date.as.factor=T)
     
     ### Soil C
     s.soilc <- treatment_effect_abs_statistics(inDF=soil_c_pool, 
-                                           var.cond="pool", var.col=3)
+                                           var.cond="pool", var.col=3,
+                                           date.as.factor=T)
     
     ### Leaf C
     s.leafc <- treatment_effect_abs_statistics(inDF=leaf_c_pool, 
-                                           var.cond="pool", var.col=3)
+                                           var.cond="pool", var.col=3,
+                                           date.as.factor=T)
     
     ### Wood C pool
     s.woodc <- treatment_effect_abs_statistics(inDF=wood_c_pool, 
-                                              var.cond="pool", var.col=3) 
+                                              var.cond="pool", var.col=3,
+                                              date.as.factor=F) 
     
     ### Fineroot C pool
     s.frc <- treatment_effect_abs_statistics(inDF=fineroot_c_pool, 
-                                         var.cond="pool", var.col=3)
+                                         var.cond="pool", var.col=3,
+                                         date.as.factor=T)
     
     ### Coarseroot C pool
     s.crc <- treatment_effect_abs_statistics(inDF=coarse_root_c_pool_1, 
-                                            var.cond="pool", var.col=3)
+                                            var.cond="pool", var.col=3,
+                                            date.as.factor=F)
     
     ### Understorey aboveground C pool
     s.uac <- treatment_effect_abs_statistics(inDF=understorey_aboveground_c_pool, 
-                                         var.cond="pool", var.col=5)
+                                         var.cond="pool", var.col=5,
+                                         date.as.factor=T)
     
     s.uac.live <- treatment_effect_abs_statistics(inDF=understorey_aboveground_c_pool, 
-                                         var.cond="pool", var.col=3)
+                                         var.cond="pool", var.col=3,
+                                         date.as.factor=T)
     
     s.uac.dead <- treatment_effect_abs_statistics(inDF=understorey_aboveground_c_pool, 
-                                         var.cond="pool", var.col=4)
+                                         var.cond="pool", var.col=4,
+                                         date.as.factor=T)
     
     s.uac2 <- treatment_effect_abs_statistics(inDF=understorey_aboveground_c_pool_2, 
-                                         var.cond="pool", var.col=3)
+                                         var.cond="pool", var.col=3,
+                                         date.as.factor=T)
     
     ### Microbial C pool
     s.micc <- treatment_effect_abs_statistics(inDF=microbial_c_pool, 
-                                          var.cond="pool", var.col=3)
+                                          var.cond="pool", var.col=3,
+                                          date.as.factor=T)
     
     ### Mycorrhizal C pool
     s.mycc <- treatment_effect_abs_statistics(inDF=mycorrhizal_c_pool, 
-                                             var.cond="pool", var.col=3)
+                                             var.cond="pool", var.col=3,
+                                             date.as.factor=T)
     
     ### Standing dead C pool
     
@@ -66,75 +79,93 @@ generate_stats_abs <- function() {
     
     ### Root respiration
     s.rroot <- treatment_effect_abs_statistics(inDF=root_respiration_flux, 
-                                              var.cond="flux", var.col=5)
+                                              var.cond="flux", var.col=5,
+                                              date.as.factor=T)
     
     ### Understorey respiration
     s.rund <- treatment_effect_abs_statistics(inDF=understorey_respiration_flux, 
-                                          var.cond="flux", var.col=5)
+                                          var.cond="flux", var.col=5,
+                                          date.as.factor=T)
     
     ### Frass production
     s.fras <- treatment_effect_abs_statistics(inDF=frass_production_flux, 
-                                             var.cond="flux", var.col=5)
+                                             var.cond="flux", var.col=5,
+                                             date.as.factor=T)
     
     ### herbivory leaf consumption flux
     s.hb.cons <- treatment_effect_abs_statistics(inDF=herbivory_leaf_consumption_flux, 
-                                             var.cond="flux", var.col=5)
+                                             var.cond="flux", var.col=5,
+                                             date.as.factor=T)
     
     ### Herbivory respiration
     s.rhb <- treatment_effect_abs_statistics(inDF=herbivory_respiration_flux, 
-                                         var.cond="flux", var.col=5)
+                                         var.cond="flux", var.col=5,
+                                         date.as.factor=T)
     
     ### Lerp production
     s.lerp.prod <- treatment_effect_abs_statistics(inDF=lerp_production_flux, 
-                                               var.cond="flux", var.col=5)
+                                               var.cond="flux", var.col=5,
+                                               date.as.factor=T)
     
     ### soil respiration
     s.rsoil <- treatment_effect_abs_statistics(inDF=soil_respiration_flux, 
-                                              var.cond="flux", var.col=5)
+                                              var.cond="flux", var.col=5,
+                                              date.as.factor=T)
     
     ### DOC leaching
     s.doc <- treatment_effect_abs_statistics(inDF=doc_leaching_flux, 
-                                         var.cond="flux", var.col=5)
+                                         var.cond="flux", var.col=5,
+                                         date.as.factor=T)
     
     ### CH4 uptake - un-gap filled data
     s.ch4 <- treatment_effect_abs_statistics(inDF=methane_c_flux, 
-                                         var.cond="flux", var.col=3)
+                                         var.cond="flux", var.col=3,
+                                         date.as.factor=T)
     
     ### Leaflitter flux
     s.lit.leaf <- treatment_effect_abs_statistics(inDF=leaflitter_flux, 
-                                                     var.cond="flux", var.col=6)  
+                                                  var.cond="flux", var.col=6,
+                                                  date.as.factor=T)  
     
     ### twig litter flux
     s.lit.twig <- treatment_effect_abs_statistics(inDF=leaflitter_flux, 
-                                             var.cond="flux", var.col=3)
+                                             var.cond="flux", var.col=3,
+                                             date.as.factor=T)
     
     ### bark litter flux
     s.lit.bark <- treatment_effect_abs_statistics(inDF=leaflitter_flux, 
-                                              var.cond="flux", var.col=4) 
+                                              var.cond="flux", var.col=4,
+                                              date.as.factor=T) 
     
     ### Seed litter flux
     s.lit.seed <- treatment_effect_abs_statistics(inDF=leaflitter_flux, 
-                                              var.cond="flux", var.col=5)
+                                              var.cond="flux", var.col=5,
+                                              date.as.factor=T)
     
     ### Wood production flux
     s.wood.prod <- treatment_effect_abs_statistics(inDF=wood_production_flux, 
-                                               var.cond="flux", var.col=5) 
+                                               var.cond="flux", var.col=5,
+                                               date.as.factor=T) 
     
     ### Fineroot production flux
     s.froot.prod <- treatment_effect_abs_statistics(inDF=fineroot_production_flux, 
-                                                var.cond="flux", var.col=5) 
+                                                var.cond="flux", var.col=5,
+                                                date.as.factor=T) 
     
     ### Coarseroot production
     s.croot.prod <- treatment_effect_abs_statistics(inDF=coarse_root_production_flux_1, 
-                                                var.cond="flux", var.col=5)
+                                                var.cond="flux", var.col=5,
+                                                date.as.factor=T)
     
     ### Understorey aboveground production
     s.und.prod <- treatment_effect_abs_statistics(inDF=understorey_aboveground_production_flux, 
-                                              var.cond="flux", var.col=5)
+                                              var.cond="flux", var.col=5,
+                                              date.as.factor=T)
     
     ### Rh respiration
     s.rh <- treatment_effect_abs_statistics(inDF=heterotrophic_respiration_flux, 
-                                        var.cond="flux", var.col=5) 
+                                        var.cond="flux", var.col=5,
+                                        date.as.factor=T) 
     
     #### Create a output table to store all stats
     var.list <- c("lai", "sla","soil_c","leaf_c","wood_c","fineroot_c",
--- R/treatment_effect_abs_statistics.R
+++ R/treatment_effect_abs_statistics.R
@@ -1,10 +1,12 @@
-treatment_effect_abs_statistics <- function(inDF, var.cond, var.col) {
+treatment_effect_abs_statistics <- function(inDF, var.cond, var.col, date.as.factor) {
     
     #### compare treatment effect of inDF variable
     #### var.cond: == flux 
     ####           == pool
     #### var.col: which variable column to extract data points
     #### Compute confidence interval of the absolute values
+    #### For fluxes, time should always be a factor
+    #### For pools that increment over time, time doesn't have to be a factor
     
     #### Assign amb and ele factor
     for (i in (1:length(inDF$Ring))) {
@@ -30,115 +32,175 @@ treatment_effect_abs_statistics <- function(inDF, var.cond, var.col) {
         ###------ Ring random factor
         ###------ Unit mg m-2 d-1
         
-        ### mixed linear model
-        ## 1. with interaction between date and treatment
-        modelt1 <- lmer(Value~Trt*Date + (1|Ring),data=inDF)
-        ## 2. without interaction between date and treatment
-        modelt2 <- lmer(Value~Trt+Date + (1|Ring),data=inDF)
-        ## 3. with interaction, date as factor
-        modelt3 <- lmer(Value~Trt*Datef + (1|Ring),data=inDF)
-        ## 4. without interaction, date as factor
-        modelt4 <- lmer(Value~Trt+Datef + (1|Ring),data=inDF)
-        
-        ### a sub-sample plot to see if date is a factor
-        #require(lattice)
-        #d.list <- unique(inDF$Date)
-        #subDF <- subset(inDF, Date %in% d.list[1:2])
-        #bwplot(Value ~ Trt | Datef, data=subDF)
-        
-        ### Different way of plotting
-        #require(sciplot)
-        #bargraph.CI(Date, Value, Trt, data=subDF, legend=TRUE)
-        
-        ### inspect individual coefficient p-values
-        ## if p<0.05, interaction is not significant
-        require(lmerTest)
-        require(pbkrtest)
-        interaction.p1 <- KRmodcomp(modelt1, modelt2)
-        interaction.p2 <- KRmodcomp(modelt3, modelt4)
-        
-        ### Visual inspection
-        #require(visreg)
-        #visreg(modelt1, "Date", by="Trt", overlay=TRUE)
-        #visreg(modelt2, "Date", by="Trt", overlay=TRUE)
-        
-        #visreg(modelt3, "Datef", by="Trt", overlay=TRUE)
-        #visreg(modelt4, "Datef", by="Trt", overlay=TRUE)
-        
-        ### AIC - smaller means better model
-        ### if p value above tells no difference between models considering interaction and not,
-        ### smaller AIC model should be picked.
-        mod.aic <- AIC(modelt1, modelt2, modelt3, modelt4)
-        
-        ### Plot fitted against residual
-        #require(car)
-        #plot(fitted(modelt1), residuals(modelt1))
-        #abline(h=0)
-        #qqPlot(residuals(modelt1))
-        #
-        #plot(fitted(modelt2), residuals(modelt2))
-        #abline(h=0)
-        #qqPlot(residuals(modelt2))
-        #
-        #plot(fitted(modelt3), residuals(modelt3))
-        #abline(h=0)
-        #qqPlot(residuals(modelt3))
-        #
-        #plot(fitted(modelt4), residuals(modelt4))
-        #abline(h=0)
-        #qqPlot(residuals(modelt4))
-        
-        ### anova
-        ### Type I, II, and III explained:
-        ### Type I tests sequential effect of A and B
-        ### Type II tests for each main effect after the other main effect
-        ### Type III tests for the presence of  a main effect after the other main effect and the interaction
-        ### If the interaction is not significance, type II is more powerful
-        m1.anova <- Anova(modelt1, test="F")
-        m2.anova <- Anova(modelt2, test="F")
-        m3.anova <- Anova(modelt3, test="F")
-        m4.anova <- Anova(modelt4, test="F")
-        # m5.anova <- Anova(modelt3, type="III", test="F")
-        
-        ### Check ele - amb diff
-        summ1 <- summary(glht(modelt1, linfct = mcp(Trt = "Tukey")))
-        summ2 <- summary(glht(modelt2, linfct = mcp(Trt = "Tukey")))
-        summ3 <- summary(glht(modelt3, linfct = mcp(Trt = "Tukey")))
-        summ4 <- summary(glht(modelt4, linfct = mcp(Trt = "Tukey")))
-        
-        ### average effect size
-        eff.size1 <- coef(modelt1)[[1]][1,2]
-        eff.size2 <- coef(modelt2)[[1]][1,2]
-        eff.size3 <- coef(modelt3)[[1]][1,2]
-        eff.size4 <- coef(modelt4)[[1]][1,2]
-        
-        ### confidence interval
-        eff.conf1 <- confint(modelt1,"Trtele")
-        eff.conf2 <- confint(modelt2,"Trtele")
-        eff.conf3 <- confint(modelt3,"Trtele")
-        eff.conf4 <- confint(modelt4,"Trtele")
-        
-        ### Generate a range of p-values for individual variables
-        #library(LMERConvenienceFunctions)
-        #modelt1.upd <- update(modelt1, REML=FALSE)
-        #modelt2.upd <- update(modelt2, REML=FALSE)
-        #modelt3.upd <- update(modelt3, REML=FALSE)
-        #modelt4.upd <- update(modelt4, REML=FALSE)
-        #
-        #p.range1 <- pamer.fnc(modelt1.upd)
-        #p.range2 <- pamer.fnc(modelt2.upd)
-        #p.range3 <- pamer.fnc(modelt3.upd)
-        #p.range4 <- pamer.fnc(modelt4.upd)
-        
-        #### dataframe with annual totals in mg m-2 yr-1
+    #    ### mixed linear model
+    #    ## 1. with interaction between date and treatment
+    #    modelt1 <- lmer(Value~Trt*Date + (1|Ring),data=inDF)
+    #    ## 2. without interaction between date and treatment
+    #    modelt2 <- lmer(Value~Trt+Date + (1|Ring),data=inDF)
+    #    ## 3. with interaction, date as factor
+    #    modelt3 <- lmer(Value~Trt*Datef + (1|Ring),data=inDF)
+    #    ## 4. without interaction, date as factor
+    #    modelt4 <- lmer(Value~Trt+Datef + (1|Ring),data=inDF)
+    #    
+    #    ### a sub-sample plot to see if date is a factor
+    #    #require(lattice)
+    #    #d.list <- unique(inDF$Date)
+    #    #subDF <- subset(inDF, Date %in% d.list[1:2])
+    #    #bwplot(Value ~ Trt | Datef, data=subDF)
+    #    
+    #    ### Different way of plotting
+    #    #require(sciplot)
+    #    #bargraph.CI(Date, Value, Trt, data=subDF, legend=TRUE)
+    #    
+    #    ### inspect individual coefficient p-values
+    #    ## if p<0.05, interaction is not significant
+    #    require(lmerTest)
+    #    require(pbkrtest)
+    #    interaction.p1 <- KRmodcomp(modelt1, modelt2)
+    #    interaction.p2 <- KRmodcomp(modelt3, modelt4)
+    #    
+    #    ### Visual inspection
+    #    #require(visreg)
+    #    #visreg(modelt1, "Date", by="Trt", overlay=TRUE)
+    #    #visreg(modelt2, "Date", by="Trt", overlay=TRUE)
+    #    
+    #    #visreg(modelt3, "Datef", by="Trt", overlay=TRUE)
+    #    #visreg(modelt4, "Datef", by="Trt", overlay=TRUE)
+    #    
+    #    ### AIC - smaller means better model
+    #    ### if p value above tells no difference between models considering interaction and not,
+    #    ### smaller AIC model should be picked.
+    #    mod.aic <- AIC(modelt1, modelt2, modelt3, modelt4)
+    #    
+    #    ### Plot fitted against residual
+    #    #require(car)
+    #    #plot(fitted(modelt1), residuals(modelt1))
+    #    #abline(h=0)
+    #    #qqPlot(residuals(modelt1))
+    #    #
+    #    #plot(fitted(modelt2), residuals(modelt2))
+    #    #abline(h=0)
+    #    #qqPlot(residuals(modelt2))
+    #    #
+    #    #plot(fitted(modelt3), residuals(modelt3))
+    #    #abline(h=0)
+    #    #qqPlot(residuals(modelt3))
+    #    #
+    #    #plot(fitted(modelt4), residuals(modelt4))
+    #    #abline(h=0)
+    #    #qqPlot(residuals(modelt4))
+    #    
+    #    ### anova
+    #    ### Type I, II, and III explained:
+    #    ### Type I tests sequential effect of A and B
+    #    ### Type II tests for each main effect after the other main effect
+    #    ### Type III tests for the presence of  a main effect after the other main effect and the interaction
+    #    ### If the interaction is not significance, type II is more powerful
+    #    m1.anova <- Anova(modelt1, test="F")
+    #    m2.anova <- Anova(modelt2, test="F")
+    #    m3.anova <- Anova(modelt3, test="F")
+    #    m4.anova <- Anova(modelt4, test="F")
+    #    # m5.anova <- Anova(modelt3, type="III", test="F")
+    #    
+    #    ### Check ele - amb diff
+    #    summ1 <- summary(glht(modelt1, linfct = mcp(Trt = "Tukey")))
+    #    summ2 <- summary(glht(modelt2, linfct = mcp(Trt = "Tukey")))
+    #    summ3 <- summary(glht(modelt3, linfct = mcp(Trt = "Tukey")))
+    #    summ4 <- summary(glht(modelt4, linfct = mcp(Trt = "Tukey")))
+    #    
+    #    ### average effect size
+    #    eff.size1 <- coef(modelt1)[[1]][1,2]
+    #    eff.size2 <- coef(modelt2)[[1]][1,2]
+    #    eff.size3 <- coef(modelt3)[[1]][1,2]
+    #    eff.size4 <- coef(modelt4)[[1]][1,2]
+    #    
+    #    ### confidence interval
+    #    eff.conf1 <- confint(modelt1,"Trtele")
+    #    eff.conf2 <- confint(modelt2,"Trtele")
+    #    eff.conf3 <- confint(modelt3,"Trtele")
+    #    eff.conf4 <- confint(modelt4,"Trtele")
+    #    
+    #    ### Generate a range of p-values for individual variables
+    #    #library(LMERConvenienceFunctions)
+    #    #modelt1.upd <- update(modelt1, REML=FALSE)
+    #    #modelt2.upd <- update(modelt2, REML=FALSE)
+    #    #modelt3.upd <- update(modelt3, REML=FALSE)
+    #    #modelt4.upd <- update(modelt4, REML=FALSE)
+    #    #
+    #    #p.range1 <- pamer.fnc(modelt1.upd)
+    #    #p.range2 <- pamer.fnc(modelt2.upd)
+    #    #p.range3 <- pamer.fnc(modelt3.upd)
+    #    #p.range4 <- pamer.fnc(modelt4.upd)
+    #    
+    #    #### dataframe with annual totals in mg m-2 yr-1
+    #    ###------ Treatment interacting with date, or not
+    #    ###------ Ring random factor
+    #    ###------ Unit mg m-2 yr-1
+    #    
+    #    ### This result is not annual total, because not everyday has a data point!
+    #    tDF <- summaryBy(Value~Trt+Ring+Yr,data=inDF,FUN=sum, keep.names=T)
+    #    tDF$Yrf <- as.factor(tDF$Yr)
+    #    
+    #    ### Analyse the variable model
+    #    ## 1. with interaction between date and treatment
+    #    modelt5 <- lmer(Value~Trt*Yr + (1|Ring),data=tDF)
+    #    ## 2. without interaction between date and treatment
+    #    modelt6 <- lmer(Value~Trt+Yr + (1|Ring),data=tDF)
+    #    ## 3. with interaction, date as factor
+    #    modelt7 <- lmer(Value~Trt*Yrf + (1|Ring),data=tDF)
+    #    ## 4. without interaction, date as factor
+    #    modelt8 <- lmer(Value~Trt+Yrf + (1|Ring),data=tDF)
+    #    
+    #    ### Check interaction significance to non-interaction model
+    #    interaction.p3 <- KRmodcomp(modelt5, modelt6)
+    #    interaction.p4 <- KRmodcomp(modelt7, modelt8)
+    #    
+    #    ### anova
+    #    m5.anova <- Anova(modelt5, test="F")
+    #    m6.anova <- Anova(modelt6, test="F")
+    #    m7.anova <- Anova(modelt7, test="F")
+    #    m8.anova <- Anova(modelt8, test="F")
+    #    
+    #    ### Check ele - amb diff
+    #    summ5 <- summary(glht(modelt5, linfct = mcp(Trt = "Tukey")))
+    #    summ6 <- summary(glht(modelt6, linfct = mcp(Trt = "Tukey")))
+    #    summ7 <- summary(glht(modelt7, linfct = mcp(Trt = "Tukey")))
+    #    summ8 <- summary(glht(modelt8, linfct = mcp(Trt = "Tukey")))
+    #    
+    #    ### average effect size
+    #    eff.size5 <- coef(modelt5)[[1]][1,2]
+    #    eff.size6 <- coef(modelt6)[[1]][1,2]
+    #    eff.size7 <- coef(modelt7)[[1]][1,2]
+    #    eff.size8 <- coef(modelt8)[[1]][1,2]
+    #    
+    #    ### confidence interval 
+    #    ### if reporting confidence interval for annual,
+    #    ### need to actually calculate annual fluxes!!!
+    #    ### Also, confidence interval is too large. It doesn't mean anything now!
+    #    eff.conf5 <- confint(modelt5,"Trtele")
+    #    eff.conf6 <- confint(modelt6,"Trtele")
+    #    eff.conf7 <- confint(modelt7,"Trtele")
+    #    eff.conf8 <- confint(modelt8,"Trtele")
+
+        #### dataframe with annual totals in g m-2 yr-1
         ###------ Treatment interacting with date, or not
         ###------ Ring random factor
-        ###------ Unit mg m-2 yr-1
+        ###------ Unit g m-2 yr-1
         
-        ### This result is not annual total, because not everyday has a data point!
+        ## Get year list and ring list
+        yr.list <- unique(inDF$Yr)
         tDF <- summaryBy(Value~Trt+Ring+Yr,data=inDF,FUN=sum, keep.names=T)
         tDF$Yrf <- as.factor(tDF$Yr)
         
+        ### Loop through data, return annual flux in g m-2 yr-1
+        for (i in 1:6) {
+            for (j in yr.list) {
+                tmp <- with(inDF[inDF$Ring == i & inDF$Yr == j, ],
+                              sum(Value*ndays, na.rm=T)/sum(ndays, na.rm=T)) * 365 / 1000 
+                tDF[tDF$Ring == i & tDF$Yr == j, "Value"] <- tmp
+            }
+        }
+
         ### Analyse the variable model
         ## 1. with interaction between date and treatment
         modelt5 <- lmer(Value~Trt*Yr + (1|Ring),data=tDF)
@@ -179,45 +241,89 @@ treatment_effect_abs_statistics <- function(inDF, var.cond, var.col) {
         eff.conf6 <- confint(modelt6,"Trtele")
         eff.conf7 <- confint(modelt7,"Trtele")
         eff.conf8 <- confint(modelt8,"Trtele")
-
-        return(list(mod = modelt2, 
-                    anova = m2.anova,
-                    diff = summ2,
-                    eff = eff.size2,
-                    conf = eff.conf2))
+        
+        
+        ### all fluxes should consider date as a factor
+        ### hene only need to compare and report best model of models 5-8
+        ### Here we need to report the CO2 by date interaction, 
+        ### hence reporting model 7
+        
+        return(list(mod = modelt7, 
+                    anova = m7.anova,
+                    diff = summ7,
+                    eff = eff.size7,
+                    conf = eff.conf7))
         
     } else if (var.cond == "pool") {
-        ## Treatment interacting with date, Ring random factor
-        modelt1 <- lmer(Value~Trt*Datef + (1|Ring),data=inDF)
-        m1.anova <- Anova(modelt1)
+        #### Analyse the variable model
+        ###------ Treatment interacting with date, or not
+        ###------ Ring random factor
+        ###------ Unit g m-2
         
-        ## Check ele - amb diff
+        ### mixed linear model
+        ## 1. with interaction between date and treatment
+        modelt1 <- lmer(Value~Trt*Date + (1|Ring),data=inDF)
+        ## 2. without interaction between date and treatment
+        modelt2 <- lmer(Value~Trt+Date + (1|Ring),data=inDF)
+        ## 3. with interaction, date as factor
+        modelt3 <- lmer(Value~Trt*Datef + (1|Ring),data=inDF)
+        ## 4. without interaction, date as factor
+        modelt4 <- lmer(Value~Trt+Datef + (1|Ring),data=inDF)
+        
+        ### inspect individual coefficient p-values
+        ## if p<0.05, interaction is not significant
+        require(lmerTest)
+        require(pbkrtest)
+        interaction.p1 <- KRmodcomp(modelt1, modelt2)
+        interaction.p2 <- KRmodcomp(modelt3, modelt4)
+        
+        ### AIC - smaller means better model
+        ### if p value above tells no difference between models considering interaction and not,
+        ### smaller AIC model should be picked.
+        mod.aic <- AIC(modelt1, modelt2, modelt3, modelt4)
+        
+        ### anova
+        ### Type I, II, and III explained:
+        ### Type I tests sequential effect of A and B
+        ### Type II tests for each main effect after the other main effect
+        ### Type III tests for the presence of  a main effect after the other main effect and the interaction
+        ### If the interaction is not significance, type II is more powerful
+        m1.anova <- Anova(modelt1, test="F")
+        m2.anova <- Anova(modelt2, test="F")
+        m3.anova <- Anova(modelt3, test="F")
+        m4.anova <- Anova(modelt4, test="F")
+
+        ### Check ele - amb diff
         summ1 <- summary(glht(modelt1, linfct = mcp(Trt = "Tukey")))
-        
-        ## average effect size
-        eff.size1 <- coef(modelt1)[[1]][1,2]
-        
-        ## confidence interval
-        eff.conf1 <- confint(modelt1,"Trtele")
-        
-        ## Model 2
-        modelt2 <- lmer(Value~Trt+Datef+(1|Ring),data=inDF)
-        m2.anova <- Anova(modelt2)
-        
-        ## Check ele - amb diff
         summ2 <- summary(glht(modelt2, linfct = mcp(Trt = "Tukey")))
+        summ3 <- summary(glht(modelt3, linfct = mcp(Trt = "Tukey")))
+        summ4 <- summary(glht(modelt4, linfct = mcp(Trt = "Tukey")))
         
-        ## average effect size
+        ### average effect size
+        eff.size1 <- coef(modelt1)[[1]][1,2]
         eff.size2 <- coef(modelt2)[[1]][1,2]
+        eff.size3 <- coef(modelt3)[[1]][1,2]
+        eff.size4 <- coef(modelt4)[[1]][1,2]
         
-        ## confidence interval
+        ### confidence interval
+        eff.conf1 <- confint(modelt1,"Trtele")
         eff.conf2 <- confint(modelt2,"Trtele")
+        eff.conf3 <- confint(modelt3,"Trtele")
+        eff.conf4 <- confint(modelt4,"Trtele")
         
-        return(list(mod = modelt2, 
-                    anova = m2.anova,
-                    diff = summ2,
-                    eff = eff.size2,
-                    conf = eff.conf2))
-    }
-    
+        
+        if (date.as.factor==T) {
+            return(list(mod = modelt3, 
+                        anova = m3.anova,
+                        diff = summ3,
+                        eff = eff.size3,
+                        conf = eff.conf3))
+        } else {
+            return(list(mod = modelt1, 
+                        anova = m1.anova,
+                        diff = summ1,
+                        eff = eff.size1,
+                        conf = eff.conf1))
+        }
+    }  # flux or pool if statement
 }
