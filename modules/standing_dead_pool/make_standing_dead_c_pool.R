babf149cc3adddb7082314ad28a7ee0630f40693
mingkaijiang <shanyujordan512@gmail.com>
2018-08-17 15:57:03 +1000

    updated mortality

--- modules/standing_dead_pool/make_standing_dead_c_pool.R
+++ modules/standing_dead_pool/make_standing_dead_c_pool.R
@@ -6,112 +6,73 @@ make_standing_dead_c_pool <- function(ring_area, c_frac) {
     download_mortality_data()
     
     ### read in mortality information
-    #morDF <- read.csv(file.path(getToPath(), "FACE_P0045_RA_MORTALITY_RAW_20150501_v1.csv"))
-    #morDF$date_last_observed_alive <- as.Date(morDF$date_last_observed_alive,format="%d/%m/%Y")
-    
     morDF <- read.csv(file.path(getToPath(), "dendro_mortality_updated.txt"))
     morDF$date_last_observed_alive <- as.Date(morDF$date_last_observed_alive,format="%d/%m/%Y")
     
-    ### read in 2012-15 data sets
-    f13 <- read.csv(file.path(getToPath(), "FACE_P0025_RA_TREEMEAS_2012-13_RAW-V1.csv"))
-    f14 <- read.csv(file.path(getToPath(), "FACE_P0025_RA_TREEMEAS_2013-14_RAW_V1.csv"))
-    f15 <- read.csv(file.path(getToPath(), "FACE_P0025_RA_TREEMEAS_2015_RAW_V1.csv"))
-    f16 <- read.csv(file.path(getToPath(), "FACE_P0025_RA_TREEMEAS_2016_RAW_V1.csv"))
-    
-    ### this file is not on HIEv yet!
-    f12 <- read.csv("temp_files/EucFACE_dendrometers2011-12_RAW.csv")
-    
-    ########################
     ### Read in additional files
-    classif1 <- read.csv("download/FACE_AUX_RA_TREE-DESCRIPTIONS_R_20130201.csv",stringsAsFactors = FALSE)
-    classif <- read.csv("data/Tree_classifications.csv",stringsAsFactors = FALSE)
-    classif$Active.FALSE.means.dead.[classif$Tree == 608] <- FALSE  # This tree dead too
-    classif2 <- merge(classif1,classif,by=c("Tree","Ring","CO2.trt"))
-    classif2 <- classif2[,c("Tree", "Ring", "CO2.trt", "Classif.x", "Height", "Diameter", "Active.FALSE.means.dead.")]
-    colnames(classif2) <- c("Tree", "Ring", "CO2.trt", "Classif", "Height", "Diameter", "Active.FALSE.means.dead.")
-    
-    ########################
-    
-    ### Merge the files
-    all <- merge(classif2,f12,by=c("Tree","Ring","CO2.trt"))
-    all <- merge(all,f13,by=c("Tree","Ring","CO2.trt")) 
-    all <- merge(all,f14,by=c("Tree","Ring","CO2.trt"))  
-    all <- merge(all,f15,by=c("Tree","Ring","CO2.trt"))
-    all <- merge(all,f16,by=c("Tree","Ring","CO2.trt"))
-    
-    ### subset dead trees
-    tree.list1 <- all$Tree[all$Active.FALSE.means.dead.=="FALSE"]
-    tree.list2 <- morDF$Tree
-    
-    subDF <- all[all$Tree %in% tree.list2, ]
+    classif <- read.csv("download/FACE_AUX_RA_TREE-DESCRIPTIONS_R_20130201.csv",stringsAsFactors = FALSE)
 
-    ### remove "CORR" columns and dead column
-    uncorr <- subDF[,-grep("CORR",names(subDF))]
-    uncorr <- uncorr[,names(uncorr) != "Active.FALSE.means.dead."]
+    ### Merge the files
+    all <- merge(classif,morDF,by=c("Tree"))
     
-    ### make a long-form version of dataframe
-    long <- reshape(uncorr,idvar="Tree",varying=list(7:58),direction="long")
-    dates <- names(uncorr)[7:58]
-    long$Date <- c(rep(Sys.Date(),length(long$time)))  
-    for (i in (1:length(long$time))) {
-        long$Date[i] <- as.Date(dates[long$time[i]],format="X%d.%m.%Y")
+    ### add biomass to long-form dataframe
+    all$biom <- allom_agb(all$last_observed_diameter)  # in kg DM
+    
+    ### sum across rings and dates
+    data.tot <- summaryBy(biom~date_last_observed_alive+Ring,data=all,FUN=sum,keep.names=T,na.rm=T)
+    
+    ## divide by ring area to get biomass per m2
+    data.tot$wood_pool <- data.tot$biom / ring_area
+    
+    ### convert from kg DM m-2 to g C m-2
+    data.tot$wood_pool <- data.tot$wood_pool * c_frac * 1000
+    colnames(data.tot)<- c("Date", "Ring", "biom","wood_pool")
+    
+    ### Calculate cumulative mass per ring per date
+    outDF <- data.frame(rep(c(1:6), length(unique(data.tot$Date))),
+                        NA, NA)
+    colnames(outDF) <- c("Ring", "Date", "wood_pool")
+    outDF$Date <- rep(unique(data.tot$Date), each=6)
+    outDF$wood_pool <- 0
+    
+    ### tmp DF
+    tmpDF <- outDF
+    date.list <- unique(data.tot$Date)
+    for (i in unique(data.tot$Ring[data.tot$Date==date.list[1]])) {
+        tmpDF$wood_pool[tmpDF$Ring==i&tmpDF$Date==date.list[1]] <- data.tot$wood_pool[data.tot$Ring==i&data.tot$Date==date.list[1]]
     }
-    long <- renameCol(long,c("X17.02.2011"),c("diam"))
+    for (i in unique(data.tot$Ring[data.tot$Date==date.list[2]])) {
+        tmpDF$wood_pool[tmpDF$Ring==i&tmpDF$Date==date.list[2]] <- data.tot$wood_pool[data.tot$Ring==i&data.tot$Date==date.list[2]]
+    }
+    for (i in unique(data.tot$Ring[data.tot$Date==date.list[3]])) {
+        tmpDF$wood_pool[tmpDF$Ring==i&tmpDF$Date==date.list[3]] <- data.tot$wood_pool[data.tot$Ring==i&data.tot$Date==date.list[3]]
+    }
+    for (i in unique(data.tot$Ring[data.tot$Date==date.list[4]])) {
+        tmpDF$wood_pool[tmpDF$Ring==i&tmpDF$Date==date.list[4]] <- data.tot$wood_pool[data.tot$Ring==i&data.tot$Date==date.list[4]]
+    }
+    for (i in unique(data.tot$Ring[data.tot$Date==date.list[5]])) {
+        tmpDF$wood_pool[tmpDF$Ring==i&tmpDF$Date==date.list[5]] <- data.tot$wood_pool[data.tot$Ring==i&data.tot$Date==date.list[5]]
+    }
+    for (i in unique(data.tot$Ring[data.tot$Date==date.list[6]])) {
+        tmpDF$wood_pool[tmpDF$Ring==i&tmpDF$Date==date.list[6]] <- data.tot$wood_pool[data.tot$Ring==i&data.tot$Date==date.list[6]]
+    }
+    for (i in unique(data.tot$Ring[data.tot$Date==date.list[7]])) {
+        tmpDF$wood_pool[tmpDF$Ring==i&tmpDF$Date==date.list[7]] <- data.tot$wood_pool[data.tot$Ring==i&data.tot$Date==date.list[7]]
+    }
     
-    long$diam <- as.numeric(long$diam)
     
-    ### Use date last seen alive and diameter information to work out biomass
-    for (i in tree.list2) {
-        alive.date <- morDF[morDF$Tree == i, "date_last_observed_alive"]
-        last.diam <- morDF[morDF$Tree == i, "last_observed_diameter"]
-        long$diam[long$Tree == i & long$Date < alive.date] <- 0.0
-        long$diam[long$Tree == i & long$Date == alive.date] <- last.diam
-        
-        if (is.na(sum(long[long$Tree == i & long$Date >= alive.date, "diam"]))) {
-            long$diam[long$Tree == i & long$Date >= alive.date] <- last.diam
+    ### assign cumulative result
+    for (i in 1:6) {
+        outDF[outDF$Ring==i&outDF$Date==date.list[1], "wood_pool"] <- tmpDF$wood_pool[tmpDF$Date==date.list[1]&tmpDF$Ring==i]
+    }
+
+    for (i in 1:6) {
+        for (j in 2:7) {
+            outDF[outDF$Ring==i&outDF$Date==date.list[j], "wood_pool"] <- tmpDF$wood_pool[tmpDF$Date==date.list[j]&tmpDF$Ring==i]+outDF[outDF$Ring==i&outDF$Date==date.list[j-1], "wood_pool"]
         }
     }
     
-    sub.long <- subset(long, diam > 0)
-    
-    ### add biomass to long-form dataframe
-    sub.long$biom <- allom_agb(sub.long$diam)  # in kg DM
-    
-    ### calculate heartwood height, 
-    ### based on Morais and Pereira. 2007. Annals of forest science
-    sub.long$heart_height <- 0.957 * as.numeric(sub.long$Height) - 2.298
+    outDF <- outDF[outDF$wood_pool>0, ]
 
-    ### calculate heartwood diameter,
-    ### based on Morais and Pereira, 2007, Annals of Forest Scienc
-    sub.long$heart_diam <- -1.411 + 0.809 * sub.long$diam
-    
-    ### calculate biomass of heartwood and sapwood
-    sub.long$heart_biom <- allom_agb(sub.long$heart_diam)
-    sub.long$sap_biom <- sub.long$biom - sub.long$heart_biom
-    
-    ### sum across rings and dates
-    data.tot <- summaryBy(biom~Date+Ring,data=sub.long,FUN=sum,keep.names=T,na.rm=T)
-    data.heart <- summaryBy(heart_biom~Date+Ring,data=sub.long,FUN=sum,keep.names=T,na.rm=T) 
-    data.sap <- summaryBy(sap_biom~Date+Ring,data=sub.long,FUN=sum,keep.names=T,na.rm=T)
-    
-    out.dat <- cbind(data.tot, data.heart$heart_biom, data.sap$sap_biom)
-    colnames(out.dat) <- c("Date", "Ring", "Tot_biom", "Heart_biom", "Sap_biom")
-    
-    ## divide by ring area to get biomass per m2
-    out.dat$wood_pool <- out.dat$Tot_biom / ring_area
-    out.dat$sap_pool <- out.dat$Sap_biom /ring_area
-    out.dat$heart_pool <- out.dat$Heart_biom /ring_area
-    
-    ### convert from kg DM m-2 to g C m-2
-    out.dat$wood_pool <- out.dat$wood_pool * c_frac * 1000
-    out.dat$sap_pool <- out.dat$sap_pool * c_frac * 1000
-    out.dat$heart_pool <- out.dat$heart_pool * c_frac * 1000    
-    
-    ### format dataframe to return
-    wood_pool <- out.dat[,c("Date", "Ring", "wood_pool", "sap_pool", "heart_pool")]
-    
-    # Only use data period 2012-2016
-    #wood_pool <- wood_pool[wood_pool$Date<="2016-12-31",]
-    
-    return(wood_pool)
+    return(outDF)
 }
