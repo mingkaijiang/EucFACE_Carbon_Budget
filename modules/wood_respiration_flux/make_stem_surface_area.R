1e2de8bcbbef4c61c55c7ae5ab8aec1355b6dd3d
mingkaijiang <shanyujordan512@gmail.com>
2019-12-20 06:31:50 +1100

    testing stem surface area

--- modules/wood_respiration_flux/make_stem_surface_area.R
+++ modules/wood_respiration_flux/make_stem_surface_area.R
@@ -4,7 +4,7 @@ make_stem_surface_area <- function(ring_area){
     download_wood_volume()
     
     # read in DF
-    myDF2 <- read.csv(file.path(getToPath(), "FACE_RA_P0037_STEMVOL-LIDAR_20150526_L2.csv")) 
+    #myDF2 <- read.csv(file.path(getToPath(), "FACE_RA_P0037_STEMVOL-LIDAR_20150526_L2.csv")) 
     myDF <- read.csv("data/lidar_data_eucface_HIEv.csv")
   
     # average by ring area
@@ -14,6 +14,13 @@ make_stem_surface_area <- function(ring_area){
                Ring = as.numeric(Ring)) %>%
         #rename(wood_surface_area = trunk_surfarea) %>%
         dplyr::select(Date, Ring, stemarea_m2)
+    
+    #outDF <- summaryBy(trunk_surfarea ~ Ring, FUN=sum, data=myDF2, keep.names=TRUE) %>%
+    #  mutate(trunk_surfarea = trunk_surfarea / ring_area,
+    #         Date = "2015-05-26",
+    #         Ring = as.numeric(Ring)) %>%
+    #  #rename(wood_surface_area = trunk_surfarea) %>%
+    #  dplyr::select(Date, Ring, trunk_surfarea)
     
     names(outDF)[3] <- "wood_surface_area"
     
