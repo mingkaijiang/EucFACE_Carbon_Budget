bcc53f846d20d44184dd11d03469e7cdbbe597a0
mingkaijiang <shanyujordan512@gmail.com>
2020-01-05 18:13:10 +1100

    constrained downloading met data code

--- modules/wood_respiration_flux/download_tair_below_canopy.R
+++ modules/wood_respiration_flux/download_tair_below_canopy.R
@@ -1,4 +1,4 @@
 download_tair_below_canopy <- function() {
-    s <- searchHIEv("FACE_R[1-6]_B1_AirVars")
+    s <- searchHIEv("FACE_R[1-6]_B1_AirVars_201[2-7]")
     downloadTOA5(hievSearch=s, maxnfiles=20000)
 }
